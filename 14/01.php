dda0882329b87524b07db1cbdbd12b26506e6962
ldiy <lorenz_reclame@outlook.be>
2020-12-15 13:46:53 +0100

    day 14 refactor

--- 14/01.php
+++ 14/01.php
@@ -1,25 +1,19 @@
 <?php
 $lines = file("input.txt", FILE_IGNORE_NEW_LINES);
 $memory = array();
-$AND_mask = 0b111111111111111111111111111111111111;
-$OR_mask = 0b000000000000000000000000000000000000;
+$AND_mask = 0;
+$OR_mask = 0;
 foreach ($lines as $line) {
     [$command, $value] = explode(' = ', $line);
 
     if ($command == "mask") {
-        $AND_mask = (binary) intval(str_replace(['X', '1'], '1', $value), 2);
-        $OR_mask = (binary) intval(str_replace(['X', '0'], '0', $value), 2);
+        $AND_mask = (binary) intval(str_replace('X', '1', $value), 2);
+        $OR_mask = (binary) intval(str_replace('X', '0', $value), 2);
     } else {
         $adress = explode('[', str_replace(']', '', $command))[1];
-        $new_value = ((intval($value) & $AND_mask) | $OR_mask);
-        if (array_key_exists($adress, $memory)) {
-            $memory[$adress] = ($memory[$adress] & $new_value) | $new_value;
-        } else {
-            $memory[$adress] = $new_value;
-        }
+        $memory[$adress] = ((intval($value) & $AND_mask) | $OR_mask);
     }
 }
 
 $part1 = array_sum($memory);
-print_r($memory);
 echo "Part 1: " . $part1 . "\n";
--- 14/02.php
+++ 14/02.php
@@ -1,86 +1,52 @@
 <?php
 
-function mask($value, $mask) {
-    $value_split = str_split($value);
-    $mask_split = str_split($mask);
-    $result = $mask_split;
+/* !! returned bits are reversed */
+function get_floating_adresses($address, $mask, &$addresses, $index = 0) {
+    if(!array_key_exists($index,$addresses)) $addresses[$index] = 0;
+    $last_mask_bit = end($mask);
+    while ($last_mask_bit != 'X' && $last_mask_bit != NULL){
+        if($last_mask_bit == '0')
+            $addresses[$index] = ($addresses[$index] << 1) | ($address & 0b1);
+        else
+            $addresses[$index] = ($addresses[$index] << 1) | 0b1;
 
-    for($i = 0; $i < count($mask_split); $i++) {
-        if ($mask_split[$i] == '0') {
-            $result[$i] = $value_split[$i];
-        }
+        array_pop($mask);
+        $last_mask_bit = end($mask);
+        $address = $address >> 1;
     }
-    return implode($result);
+    if($last_mask_bit == 'X'){
+        $next_index = count($addresses);
+
+        $addresses[$next_index] = ($addresses[$index] << 1) | 0b1;
+        $addresses[$index] = ($addresses[$index] << 1) | 0b0;
+
+        array_pop($mask);
+        $address = $address >> 1;
+
+        get_floating_adresses($address, $mask, $addresses, $index);
+        get_floating_adresses($address, $mask, $addresses, $next_index);
+    }
 }
 
-function bin_addresses($length, $pos, &$addresses) {
-    static $bit = [];
-    static $source = ['1', '0'];
-    $pos++;
-    for($i = 0; $i < 2; $i++) {
-        $bit[$pos] = $source[$i];
-        if ($pos < $length - 1) {
-            bin_addresses($length, $pos, $addresses);
+
+function part2($input_lines) {
+    $mem = array();
+    $mask = "";
+    foreach ($input_lines as $line) {
+        [$command, $value] = explode(' = ', $line);
+
+        if ($command == "mask") {
+            $mask = $value;
         } else {
-            array_push($addresses,$bit);
+            $address =  explode('[', str_replace(']', '', $command))[1];
+            $addresses = array();
+            get_floating_adresses($address, str_split($mask), $addresses);
+            foreach ($addresses as $adr)
+                $mem[$adr] = $value;
         }
     }
-}
-function addresses($address) {
-    $address_split = str_split($address);
-    $floating_count = array_count_values($address_split)['X'];
-    $addresses = array();
-    $floating_vals = array();
-    bin_addresses($floating_count, -1 , $floating_vals);
-    for($i = 0; $i< pow(2,$floating_count); $i++) {
-        $new_address = $address_split;
-        $floating_vals_i = 0;
-        for($j = 0; $j < count($address_split); $j++){
-            if ($address_split[$j] == 'X'){
-                $new_address[$j] = $floating_vals[$i][$floating_vals_i++];
-            }
-        }
-        array_push($addresses, implode($new_address));
-    }
-    return $addresses;
+    return array_sum($mem);
 }
 
-function bin($n, &$val)
-{
-    if ($n > 1)
-        bin($n>>1, $val);
-    $val .= ($n & 1);
-}
-
-
-$lines = file("input.txt", FILE_IGNORE_NEW_LINES);
-$memory = array();
-$mask = "";
-foreach ($lines as $line) {
-    [$command, $value] = explode(' = ', $line);
-
-    if ($command == "mask") {
-        $mask = $value;
-    } else {
-        $address = explode('[', str_replace(']', '', $command))[1];
-        $new_addr = "";
-        bin(intval($address), $new_addr);
-        $new_addr = str_repeat('0', 36 - strlen($new_addr)) . $new_addr;
-        $masked_address = mask($new_addr, $mask);
-        $addresses = addresses($masked_address);
-        $new_val = "";
-        bin(intval($value), $new_val);
-        $new_val = str_repeat('0', 36 - strlen($new_val)) . $new_val;
-        foreach ($addresses as $adr) {
-            if (array_key_exists($adr, $memory)) {
-                $memory[$adr] = ($memory[$adr] & $value) | $value;
-            } else {
-                $memory[$adr] = $value;
-            }
-        }
-    }
-}
-
-$part2 = array_sum($memory);
-//print_r($memory);
-echo "Part 2: " . $part2 . "\n";
+$input = file("input.txt", FILE_IGNORE_NEW_LINES);
+echo "Part 2: " . part2($input) . "\n";
